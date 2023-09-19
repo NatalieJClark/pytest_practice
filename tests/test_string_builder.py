0c2fc74fd5b4508eb7f4ad833d4f0eb078fd3623
Natalie Clark <nj_clark@hotmail.co.uk>
2023-09-19 14:34:02 +0100

    Added StringBuilder class

---
+++ tests/test_string_builder.py
@@ -0,0 +1,47 @@
+from lib.string_builder import StringBuilder
+
+"""
+Initially the output is an empty string
+"""
+def test_initially_output_is_an_empty_string():
+    string_builder = StringBuilder()
+    assert string_builder.output() == ""
+
+"""
+When we add a single string
+The output is now that string
+"""
+def test_adding_a_string_outputs_that_string():
+    string_builder = StringBuilder()
+    string_builder.add("hello")
+    assert string_builder.output() == "hello"
+
+
+"""
+When we add a single string
+The size reflects the size of that string
+"""
+def test_adding_a_string_sets_size_to_that_strings_size():
+    string_builder = StringBuilder()
+    string_builder.add("hello")
+    assert string_builder.size() == 5
+
+"""
+When we add multiple strings
+The output is now all the added strings
+"""
+def test_adding_a_string_outputs_that_string():
+    string_builder = StringBuilder()
+    string_builder.add("hello")
+    string_builder.add("world")
+    assert string_builder.output() == "helloworld"
+
+"""
+When we add multiple strings
+The size reflects the total size of all the added strings
+"""
+def test_adding_a_string_sets_size_to_that_strings_size():
+    string_builder = StringBuilder()
+    string_builder.add("hello")
+    string_builder.add("world")
+    assert string_builder.size() == 10
